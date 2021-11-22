970a8f3e71899b188ec351bdf079aaeb574edd3b
kartik <kabakshi@hotmail.ca>
2021-11-22 17:37:43 -0500

    students that don't have an email or phone number

--- 0_selects/1_students_without_github.sql
+++ 0_selects/1_students_without_github.sql
@@ -2,3 +2,10 @@ SELECT id, name, email, cohort_id
 FROM students
 WHERE github IS NULL
 ORDER BY cohort_id
+
+-- compass asnwer
+
+-- SELECT id, name 
+-- FROM students 
+-- WHERE cohort_id = 1
+-- ORDER BY name;
---
+++ 1_queries/3_student_info.sql
@@ -0,0 +1,12 @@
+SELECT name, id
+FROM students
+WHERE email IS NULL
+OR phone IS NULL
+
+
+
+-- compass answer
+-- SELECT name, id, cohort_id
+-- FROM students
+-- WHERE email IS NULL
+-- OR phone IS NULL;
