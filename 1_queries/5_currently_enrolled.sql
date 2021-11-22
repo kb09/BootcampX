744d6f431e3bb9bec2918d2ef8486376cb2f41d0
kartik <kabakshi@hotmail.ca>
2021-11-22 17:52:44 -0500

    students currently enrolled

---
+++ 1_queries/5_currently_enrolled.sql
@@ -0,0 +1,11 @@
+SELECT name, id, cohort_id
+FROM students
+WHERE end_date IS NULL
+
+
+-- Compass answer
+
+-- SELECT name, id, cohort_id
+-- FROM students
+-- WHERE end_date IS NULL
+-- ORDER BY cohort_id;
